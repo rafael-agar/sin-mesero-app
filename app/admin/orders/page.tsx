bd149ff01e08280a481a74c6307156c7db43ee67
Rafael Agar <i.t.rafaelagar@gmail.com>
2024-08-19 23:49:05 -0400

    adding page.tsx in /admin/orders

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import useSWR from 'swr'
+import OrderCard from '@/components/order/OrderCard'
+import Heading from '@/components/ui/Heading'
+import { OrderWithProduct } from '@/src/types'
+
+export default function Page() {
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+  const {data, error, isLoading} = useSWR<OrderWithProduct[]>(url, fetcher,{refreshInterval: 100000, revalidateOnFocus: false})
+  // console.log(JSON.stringify(orders, null, 2))
+
+  if (isLoading) return <p>...Cargando Ordenes...</p>
+
+  if (data) return (
+    <>
+      <Heading>Administrando Ordenes</Heading>
+
+      {data.length ? 
+      (
+        <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'>
+          {data.map(order => (
+            <OrderCard key={order.id} order={order} />
+          ))}
+        </div>
+      ) :
+        <p className= "text-center">No hay ordenes pendientes</p>
+      }
+
+    </>
+  )
+}
